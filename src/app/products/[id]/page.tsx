e00f1e2c9d575f5fc58e53b7e61bc0073484e2ac
eman-NEMO <emanelkaser@gmail.com>
2025-02-01 15:08:18 +0200

    routing
    try dynamic routing

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,22 @@
+
+type Props = {
+  params: { id: string };
+};
+
+export default function product({ params }: Props) {
+  const { id } = params;
+
+  // Mock Data (Replace with API call or database fetch)
+  const products = [
+    { id: "1", name: "Laptop", price: "$999" },
+    { id: "2", name: "Smartphone", price: "$699" },
+  ];
+  
+  const product = products.find((p) => p.id === id);
+  return (
+    <div>
+      <h1>{product?.name}</h1>
+      <p>Price: {product?.price}</p>
+    </div>
+  );
+}
