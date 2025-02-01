d7d2333c7a3adfdf182883a469ad10ef6c370a80
eman-NEMO <emanelkaser@gmail.com>
2025-02-01 22:00:59 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["fakestoreapi.com"], // Add allowed image domains
   },
+  eslint: {
+    ignoreDuringBuilds: true, 
+  },
 };
 
 module.exports = nextConfig;
